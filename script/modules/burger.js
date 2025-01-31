832283c737f1cbb9b6b3b09d137552f78b2846e6
Andrey Kostin <andrey.kostin.111@mail.ru>
2025-02-01 00:13:31 +0300

    [M3][TASK-4.2] Add burger animation

--- script/modules/burger.js
+++ script/modules/burger.js
@@ -3,23 +3,58 @@ export const burgerMenu = () => {
   const menuButton = document.querySelector('.header__menu-button');
   const menuLinks = document.querySelectorAll('.header__link');
 
-  const toggleMenu = () => {
-    menu.classList.toggle('header__menu_active');
+  let isOpen = false;
+  let animationFrame;
+
+  const animateMenu = (open) => {
+    let start = NaN;
+    const duration = 300;
+    const burgerOffset = 200;
+    const burgerWidthOffset = 300;
+    const leftEdge = menuButton.offsetLeft;
+    console.log('leftEdge: ', leftEdge);
+    const startPosition = open ? 
+      leftEdge - burgerWidthOffset : leftEdge - burgerOffset;
+    const endPosition = open ? 
+      leftEdge - burgerOffset : leftEdge - burgerWidthOffset;
+
+    const step = (timestamp) => {
+      start ||= timestamp;
+      const progress = Math.min((timestamp - start) / duration, 1);
+      const currentPosition = startPosition + (endPosition - startPosition) * progress;
+
+      menu.style.left = `${currentPosition}px`;
+
+      if (progress < 1) {
+        animationFrame = requestAnimationFrame(step);
+      } else {
+        cancelAnimationFrame(animationFrame);
+        if (!open) menu.classList.remove('header__menu_active');
+      }
+    };
+
+    if (open) menu.classList.add('header__menu_active');
+    requestAnimationFrame(step);
   };
 
-  menuButton.addEventListener('click', (event) => {
-    toggleMenu();
-  });
+  const toggleMenu = () => {
+    isOpen = !isOpen;
+    animateMenu(isOpen);
+  };
+
+  menuButton.addEventListener('click', toggleMenu);
 
   menuLinks.forEach((link) => {
     link.addEventListener('click', () => {
-      menu.classList.remove('header__menu_active');
+      isOpen = false;
+      animateMenu(isOpen);
     })
   });
 
   document.addEventListener('click', (event) => {
     if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
-      menu.classList.remove('header__menu_active');
+      isOpen = false;
+      animateMenu(isOpen);
     }
   });
 };
